a37711d78e9729e7e50652cbc0c6e83bf5477052
Ruben <rwoldhui@avans.nl>
2020-11-17 11:16:23 +0100

    [Added] todo's

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import pandas as pd
+
+df = pd.read_csv("./data/course_061120.csv")
+print(df.head(50))
+
+# TODO: Return top 5 recommendations with confidence level > 0.3
+
+# TODO: Group object in Clusters (UI?)
+
+# TODO: Plot objects per cluster (UI)
+
+# TODO: Create object locator (UI) (search bar)
+
+# TODO: Zoom in on object (UI)
